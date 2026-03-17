08193d71087c50743aec0de2ef1a84d86d44a618
Ankur Chopra <dataengineerankur@gmail.com>
2026-03-18 00:19:04 +0530

    PATCHIT: PATCHIT agentic fix: KeyError (18318db1) (#121)

--- scripts/glue/gold_orders.py
+++ scripts/glue/gold_orders.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from awsglue.context import GlueContext
 from awsglue.job import Job
 from awsglue.utils import getResolvedOptions
@@ -9,11 +10,15 @@ args = getResolvedOptions(
     sys.argv,
     [
         "JOB_NAME",
-        "S3_BUCKET",
-        "SILVER_PREFIX",
-        "GOLD_PREFIX",
     ],
 )
+
+optional_args = {}
+for arg in ["S3_BUCKET", "SILVER_PREFIX", "GOLD_PREFIX"]:
+    for i, argv in enumerate(sys.argv):
+        if argv == f"--{arg}" and i + 1 < len(sys.argv):
+            optional_args[arg] = sys.argv[i + 1]
+            break
 
 sc = SparkContext.getOrCreate()
 glue_context = GlueContext(sc)
@@ -21,12 +26,12 @@ spark = glue_context.spark_session
 job = Job(glue_context)
 job.init(args["JOB_NAME"], args)
 
-bucket = args["S3_BUCKET"]
-silver_prefix = args["SILVER_PREFIX"].rstrip("/")
-gold_prefix = args["GOLD_PREFIX"].rstrip("/")
+bucket = optional_args.get("S3_BUCKET", os.environ.get("S3_BUCKET", ""))
+silver_prefix = optional_args.get("SILVER_PREFIX", os.environ.get("SILVER_PREFIX", "silver")).rstrip("/")
+gold_prefix = optional_args.get("GOLD_PREFIX", os.environ.get("GOLD_PREFIX", "gold")).rstrip("/")
 
-orders = spark.read.parquet(f"s3://{bucket}/{silver_prefix}/stg_orders/")
-order_items = spark.read.parquet(f"s3://{bucket}/{silver_prefix}/stg_order_items/")
+orders = spark.read.parquet(f"s3://{bucket}/{silver_prefix}/orders/")
+order_items = spark.read.parquet(f"s3://{bucket}/{silver_prefix}/order_items/")
 
 items_agg = (
     order_items.groupBy("order_id")
